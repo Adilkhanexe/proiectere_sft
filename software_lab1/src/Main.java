fc34c15105f1933deae7d3cf9c0783697a2bb930
Adilkhan <adilamen202@gmail.com>
2026-03-11 08:28:33 +0200

    Add files via upload

---
+++ software_lab1/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student("100", "Vasile", "Ionescu", "Tehnologia Informatie");
+        System.out.println(s1.toString());
+    }
+}
---
+++ software_lab1/src/Student.java
@@ -0,0 +1,16 @@
+class Student {
+    private String numarMatricol;
+    private String prenume;
+    private String nume;
+    private String formatieDeStudiu;
+
+    Student(String numarMatricol, String prenume, String nume, String formatieDeStudiu) {
+        this.numarMatricol = numarMatricol;
+        this.prenume = prenume;
+        this.nume = nume;
+        this.formatieDeStudiu = formatieDeStudiu;
+    }
+    public String toString() {
+        return nume + " " + prenume + " (ID: " + numarMatricol + ") - " + formatieDeStudiu;
+    }
+}
